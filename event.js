9f0018a832346bf7c20590d6c33d457135dc9275
Kichan <rlcks01537@gmail.com>
2024-01-23 14:31:54 +0900

    puppeteer pokemonKorea 40 event request

--- event.js
+++ event.js
@@ -1,44 +1,42 @@
 import express from "express";
-import * as cheerio from "cheerio";
 import puppeteer from "puppeteer";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    // Puppeteer를 사용하여 브라우저를 실행
-    const browser = await puppeteer.launch({ headless: "true" });
+    const browser = await puppeteer.launch({ headless: "new" });
     const page = await browser.newPage();
 
     // 해당 URL로 이동
     await page.goto("https://pokemonkorea.co.kr/news", { waitUntil: "domcontentloaded" });
 
-    // Puppeteer의 waitForSelector를 사용하여 페이지가 완전히 로드될 때까지 대기
     await page.waitForSelector("#newslist li");
 
-    // 페이지의 HTML을 가져오기
-    const htmlString = await page.content();
+    // "더보기" 버튼 클릭
+    await page.click(".btn-details");
 
-    // Cheerio를 사용하여 HTML을 파싱
-    const $ = cheerio.load(htmlString);
+    await page.waitForTimeout(2000);
 
     const eventData = [];
 
-    // 각 li 요소를 반복하며 정보 추출
-    $("#newslist li").each((index, element) => {
-      const title = $(element).find(".bx-txt h3").text().trim();
-      const date = $(element).find(".list-split li:last-child").text().trim();
-      // 이미지 URL을 가져오기 전에 해당 요소가 존재하는지 확인
-      const imageUrlElement = $(element).find(".tumb-wrp img");
-const imageUrl = imageUrlElement.length ? imageUrlElement.attr("src").trim() : null;
+    const eventList = await page.$$("#newslist li");
+    for (const element of eventList) {
+      const linkElement = await element.$("a");
 
-      eventData.push({ title, date, imageUrl });
-    });
+      if (linkElement) {
+        const link = await (await linkElement.getProperty("href")).jsonValue();
+        const title = await (await element.$(".bx-txt h3")).evaluate((node) => node.textContent.trim());
+        const sprite = await (await element.$(".list-split li:first-child")).evaluate((node) => node.textContent.trim());
+        const date = await (await element.$(".list-split li:last-child")).evaluate((node) => node.textContent.trim());
+        // 이미지 URL을 가져오기 전에 해당 요소가 존재하는지 확인
+        const imageUrlElement = await element.$(".tumb-wrp img");
+        const imageUrl = imageUrlElement ? await (await imageUrlElement.getProperty("src")).jsonValue() : null;
 
-    console.log(`Number of elements: ${$("#newslist li").length}`);
-    console.log(eventData);
+        eventData.push({ link, title, sprite, date, imageUrl });
+      }
+    }
 
-    // 브라우저 종료
     await browser.close();
 
     res.status(200).json(eventData);
