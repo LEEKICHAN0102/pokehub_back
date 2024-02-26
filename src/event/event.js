ed49d882edd6a6e66f2c3b412138378b8e5b336c
Kichan <rlcks01537@gmail.com>
2024-02-26 15:38:22 +0900

    new headless https://developer.chrome.com/articles/new-headless/

--- src/event/event.js
+++ src/event/event.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   try {
     const browser = await puppeteer.launch({ 
-      headless: true,
+      headless: "new",
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
