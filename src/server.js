22c837285fb90d1d9bbaa45155909041bb7bb813
Kichan <rlcks01537@gmail.com>
2024-03-04 15:31:09 +0900

    domain

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ app.use(session({
     secure: true,
     sameSite: "none",
     path: "/",
-    domain: "https://pokehub-encyclopedia.site",
+    domain: "pokehub-encyclopedia.site",
   },
   store: MongoStore.create({
     mongoUrl: process.env.DB_URL,
