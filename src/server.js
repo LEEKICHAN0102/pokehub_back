549b6e292db2f04fa51b1e426f45b864bdfc6957
Kichan <rlcks01537@gmail.com>
2024-03-11 13:21:04 +0900

    origin main

--- src/server.js
+++ src/server.js
@@ -29,6 +29,8 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     maxAge: 1000 * 60 * 60 * 24,
+    secure: true,
+    sameSite: "none",
   },
   store: MongoStore.create({
     mongoUrl: process.env.DB_URL,
