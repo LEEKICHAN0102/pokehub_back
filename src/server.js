600a7555293784488103c1efb898b09e4b97a14b
Kichan <rlcks01537@gmail.com>
2024-03-11 13:15:21 +0900

    none secure

--- src/server.js
+++ src/server.js
@@ -29,9 +29,6 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     maxAge: 1000 * 60 * 60 * 24,
-    secure: true,
-    sameSite: "none",
-    domain: ".pokehub-encyclopedia.site",
   },
   store: MongoStore.create({
     mongoUrl: process.env.DB_URL,
