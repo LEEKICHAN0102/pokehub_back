a4f4f60c0dd8bd1ede4899796511eabbe5d3b48c
LEEKICHAN0102 <rlcks01537@gmail.com>
2024-11-22 09:29:00 +0900

    확장 명

--- src/api/gym-leader.js
+++ src/api/gym-leader.js
@@ -5,10 +5,9 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const gymLeaders = await GymLeader.find().limit(0);
+    const gymLeaders = await GymLeader.find();
     res.json(gymLeaders);
   } catch (error) {
-    console.error("에러 발생:", error);
     res.status(500).send("서버 에러");
   }
 });
@@ -16,7 +15,7 @@ router.get("/", async (req, res) => {
 router.get("/detail/:order", async (req, res) => {
   try {
     const { order } = req.params;
-    const findByGymLeaderOrder = await GymLeader.findOne({ order: Number(order) });
+    const findByGymLeaderOrder = await mongoose.connection.collection("gym-leader").findOne({ order: Number(order) });
     res.json(findByGymLeaderOrder);
   } catch (error) {
     console.error("에러 발생:", error);
