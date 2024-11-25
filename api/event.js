c8141f53fa8d7c45e70a4fd5d3bfeff836f7b2cd
LEEKICHAN0102 <rlcks01537@gmail.com>
2024-11-25 13:38:27 +0900

    options --disable-dev-shm-usage

--- api/event.js
+++ api/event.js
@@ -1,6 +1,6 @@
 import express from "express";
 import puppeteer from "puppeteer-core";
-import chromium from "chrome-aws-lambda"; // 배포 환경에서만 필요
+import chromium from "chrome-aws-lambda";
 
 const router = express.Router();
 
@@ -11,15 +11,19 @@ router.get("/event", async (req, res) => {
     const browser = await puppeteer.launch({
       headless: true,
       executablePath: executablePath,
-      args: process.env.NODE_ENV === 'production' ? chromium.args : ['--no-sandbox', '--disable-setuid-sandbox'],
-      defaultViewport: process.env.NODE_ENV === 'production' ? chromium.defaultViewport : undefined,
+      args: [
+        ...chromium.args,
+        '--disable-dev-shm-usage', // 공유 메모리 사용 비활성화
+      ],
+      defaultViewport: chromium.defaultViewport,
     });
 
     const page = await browser.newPage();
     await page.goto("https://pokemonkorea.co.kr/news", { waitUntil: "domcontentloaded" });
 
-    await page.waitForSelector("#newslist li");
-    await page.waitForTimeout(2000);
+    // 요소가 로드될 때까지 대기
+    await page.waitForSelector("#newslist li", { timeout: 10000 });
+    await page.waitForTimeout(2000); // 추가 대기 시간
 
     const eventData = [];
     const eventList = await page.$$("#newslist li");
@@ -44,7 +48,7 @@ router.get("/event", async (req, res) => {
     res.status(200).json(eventData);
   } catch (error) {
     console.error("웹 스크래핑 에러:", error);
-    res.status(404).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 });
 
