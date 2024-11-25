e7b1d35c569998fb5349a962a35bc00dbb987274
LEEKICHAN0102 <rlcks01537@gmail.com>
2024-11-25 14:49:36 +0900

    lambda => @sparticuz

--- api/event.js
+++ api/event.js
@@ -1,29 +1,24 @@
 import express from "express";
 import puppeteer from "puppeteer-core";
-import chromium from "chrome-aws-lambda";
+import chromium from "@sparticuz/chromium";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
     const browser = await puppeteer.launch({
-      headless: true,
+      headless: chromium.headless,
       executablePath: await chromium.executablePath,
       args: [
         ...chromium.args,
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-gpu',
-        '--disable-software-rasterizer',
-        '--headless',
-        '--single-process',
-        '--disable-dev-shm-usage',
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-gpu",
+        "--disable-software-rasterizer",
       ],
-      defaultViewport: chromium.defaultViewport,
     });
 
     const page = await browser.newPage();
-    
     await page.goto("https://pokemonkorea.co.kr/news", { waitUntil: "domcontentloaded" });
 
     await page.waitForSelector("#newslist li", { timeout: 10000 });
@@ -36,12 +31,20 @@ router.get("/", async (req, res) => {
 
       if (linkElement) {
         const link = await (await linkElement.getProperty("href")).jsonValue();
-        const title = await (await element.$(".bx-txt h3")).evaluate((node) => node.textContent.trim());
-        const sprite = await (await element.$(".list-split li:first-child")).evaluate((node) => node.textContent.trim());
-        const date = await (await element.$(".list-split li:last-child")).evaluate((node) => node.textContent.trim());
-        
+        const title = await (
+          await element.$(".bx-txt h3")
+        ).evaluate((node) => node.textContent.trim());
+        const sprite = await (
+          await element.$(".list-split li:first-child")
+        ).evaluate((node) => node.textContent.trim());
+        const date = await (
+          await element.$(".list-split li:last-child")
+        ).evaluate((node) => node.textContent.trim());
+
         const imageUrlElement = await element.$(".tumb-wrp img");
-        const imageUrl = imageUrlElement ? await (await imageUrlElement.getProperty("src")).jsonValue() : null;
+        const imageUrl = imageUrlElement
+          ? await (await imageUrlElement.getProperty("src")).jsonValue()
+          : null;
 
         eventData.push({ link, title, sprite, date, imageUrl });
       }
