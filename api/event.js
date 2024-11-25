63805ce0c07f30fb0c2ae7ee4e0e4190f76e7b52
LEEKICHAN0102 <rlcks01537@gmail.com>
2024-11-25 12:59:10 +0900

    puppeteer-core & chrome-aws-lambda

--- api/event.js
+++ api/event.js
@@ -1,26 +1,29 @@
 import express from "express";
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
+import chromium from "chrome-aws-lambda"; // 배포 환경에서만 필요
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/event", async (req, res) => {
   try {
-    const browser = await puppeteer.launch({ 
-      headless: "new",
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
-    const page = await browser.newPage();
+    const executablePath = await chromium.executablePath;
 
-    // 해당 URL로 이동
+    const browser = await puppeteer.launch({
+      headless: true,
+      executablePath: executablePath,
+      args: process.env.NODE_ENV === 'production' ? chromium.args : ['--no-sandbox', '--disable-setuid-sandbox'],
+      defaultViewport: process.env.NODE_ENV === 'production' ? chromium.defaultViewport : undefined,
+    });
+
+    const page = await browser.newPage();
     await page.goto("https://pokemonkorea.co.kr/news", { waitUntil: "domcontentloaded" });
 
     await page.waitForSelector("#newslist li");
-
     await page.waitForTimeout(2000);
 
     const eventData = [];
-
     const eventList = await page.$$("#newslist li");
+
     for (const element of eventList) {
       const linkElement = await element.$("a");
 
@@ -29,7 +32,7 @@ router.get("/", async (req, res) => {
         const title = await (await element.$(".bx-txt h3")).evaluate((node) => node.textContent.trim());
         const sprite = await (await element.$(".list-split li:first-child")).evaluate((node) => node.textContent.trim());
         const date = await (await element.$(".list-split li:last-child")).evaluate((node) => node.textContent.trim());
-        // 이미지 URL을 가져오기 전에 해당 요소가 존재하는지 확인
+        
         const imageUrlElement = await element.$(".tumb-wrp img");
         const imageUrl = imageUrlElement ? await (await imageUrlElement.getProperty("src")).jsonValue() : null;
 
@@ -38,7 +41,6 @@ router.get("/", async (req, res) => {
     }
 
     await browser.close();
-
     res.status(200).json(eventData);
   } catch (error) {
     console.error("웹 스크래핑 에러:", error);
